da2df74e746ee3453e3bc1c93691d280e8d78863
Diego Fischer <diegof01gm@gmail.com>
2024-10-15 00:05:34 -0300

    commit hp

--- Registro-Automotor/main.ts
+++ Registro-Automotor/main.ts
@@ -41,3 +41,5 @@ Registro1.agregarVehiculo(auto1);
 Registro1.agregarVehiculo(camion1);
 Registro1.agregarVehiculo(auto2);
 Registro1.listarVehiculos();
+
+// todo esto lo tengo que hacer desde dentro de las clases.
